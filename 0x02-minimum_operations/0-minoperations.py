a22d2fb3f3df539b0d5709c101e4e2f80443ffea
SaidLamghari <said.lmg@gmail.com>
2024-07-11 14:21:19 +0000

    pycodestyle

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,14 +6,15 @@ en utilisant uniquement les opérations 'Tout Copier' et 'Coller'.
 Auteur SAID LAMGHARI
 """
 
+
 def minOperations(n):
     """
     Calcule le nombre minimum d'opérations nécessaires
     pour obtenir exactement n caractères H.
-    
+
     Paramètres:
     n (int): Le nombre cible de caractères H.
-    
+
     Retourne:
     int: Le nombre minimum d'opérations, ou 0 si n est impossible à atteindre.
     """
@@ -21,12 +22,12 @@ def minOperations(n):
     # de l'atteindre avec les opérations données
     if (n <= 1):
         return 0
-    
+
     # Initialisation des opérations à 0
     operatns = 0
     # Débuter avec le plus petit facteur premier
     fctr = 2
-    
+
     # Boucle tant que n est supérieur à 1
     while n > 1:
         # Vérifier si n est divisible par le facteur courant
@@ -37,5 +38,5 @@ def minOperations(n):
             n //= fctr
         # Passer au facteur suivant
         fctr += 1
-        
+
     return operatns
