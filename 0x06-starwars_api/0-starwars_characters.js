6f0af632d3cd732a61452b56fcf1e4ce805d5b46
SaidLamghari <said.lmg@gmail.com>
2024-08-08 19:30:58 +0000

    0-starwars_characters.js

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,49 @@
+#!/usr/bin/node
+
+// Importation du module 'request' pour faire des requêtes HTTP
+const request = require('request');
+
+// Récupération de l'ID du film à partir
+// des arguments de la ligne de commande
+// Exemple: Si vous exécutez
+// `./0-starwars_characters.js 3`, 'movieId' vaudra '3'
+const mveId = process.argv[2];
+
+// Construction de l'URL de l'API en utilisant l'ID du film
+// L'URL finale ressemblera à ceci: 'https://swapi.dev/api/films/3/'
+const api_lnk = `https://swapi.dev/api/films/${mveId}/`;
+
+// Envoi d'une requête GET à l'API pour récupérer les détails du film
+request(api_lnk, async function (error, response, body) {
+    // Vérification de l'existence d'une erreur dans la requête
+    if (error) return console.log(error);
+
+    // Analyse du corps de la réponse pour
+    // obtenir les données sous forme d'objet JSON
+    const infdata = JSON.parse(body);
+
+    // Extraction de la liste des URLs des personnages du film
+    const characters = infdata.characters;
+
+    // Boucle asynchrone pour parcourir chaque URL de personnage
+    for (const link of characters) {
+        // Utilisation d'une promesse pour gérer l'ordre des requêtes
+        await new Promise((resolve) => {
+            // Envoi d'une requête GET pour chaque URL de personnage
+            request(link, (error, response, body) => {
+                // Vérification de l'existence d'une erreur dans la requête
+                if (error) return console.log(error);
+
+                // Analyse du corps de la réponse
+                // pour obtenir les détails du personnage
+                const character = JSON.parse(body);
+
+                // Affichage du nom du personnage
+                console.log(character.name);
+
+                // Résolution de la promesse pour passer au personnage suivant
+                resolve();
+            });
+        });
+    }
+});
