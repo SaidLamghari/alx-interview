be04be6f4e7f672718efb1998139d998bae7c3e1
SaidLamghari <said.lmg@gmail.com>
2024-08-08 21:08:24 +0100

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,39 +3,35 @@
 // Importation du module 'request' pour faire des requêtes HTTP
 const request = require('request');
 
-// Récupération de l'ID du film à partir
-// des arguments de la ligne de commande
-// Exemple: Si vous exécutez
-// `./0-starwars_characters.js 3`, 'movieId' vaudra '3'
-const mveId = process.argv[2];
+// Récupération de l'ID du film à partir des arguments de la ligne de commande
+// Exemple: Si vous exécutez `./0-starwars_characters.js 3`, 'movieId' vaudra '3'
+const movieId = process.argv[2];
 
 // Construction de l'URL de l'API en utilisant l'ID du film
 // L'URL finale ressemblera à ceci: 'https://swapi.dev/api/films/3/'
-const apilnk = `https://swapi.dev/api/films/${mveId}/`;
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
 // Envoi d'une requête GET à l'API pour récupérer les détails du film
-request(apilnk, async function (error, response, body) {
+request(apiUrl, async function (error, response, body) {
   // Vérification de l'existence d'une erreur dans la requête
   if (error) return console.log(error);
 
-  // Analyse du corps de la réponse pour
-  // obtenir les données sous forme d'objet JSON
-  const infdata = JSON.parse(body);
+  // Analyse du corps de la réponse pour obtenir les données sous forme d'objet JSON
+  const data = JSON.parse(body);
 
   // Extraction de la liste des URLs des personnages du film
-  const characters = infdata.characters;
+  const characters = data.characters;
 
   // Boucle asynchrone pour parcourir chaque URL de personnage
-  for (const link of characters) {
+  for (const url of characters) {
     // Utilisation d'une promesse pour gérer l'ordre des requêtes
     await new Promise((resolve) => {
       // Envoi d'une requête GET pour chaque URL de personnage
-      request(link, (error, response, body) => {
+      request(url, (error, response, body) => {
         // Vérification de l'existence d'une erreur dans la requête
         if (error) return console.log(error);
 
-        // Analyse du corps de la réponse
-        // pour obtenir les détails du personnage
+        // Analyse du corps de la réponse pour obtenir les détails du personnage
         const character = JSON.parse(body);
 
         // Affichage du nom du personnage
