69f516b31b95853718dd312e929cb13de325a83d
SaidLamghari <said.lmg@gmail.com>
2024-08-22 21:17:38 +0000

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,74 +1,61 @@
 #!/usr/bin/python3
-"""Auteur SAID AMGHARI"""
-
+"""Module pour la gestion des pièces de monnaie.
+Ce module contient une fonction pour déterminer
+le nombre minimum de pièces
+nécessaires pour obtenir un montant donné en
+utilisant un ensemble de pièces de différentes valeurs.
+Auteur SAID LAMGHARI
+"""
 
 def makeChange(coins, total):
     """
-    Calcule le nombre minimum de pièces nécessaires
-    pour obtenir un montant donné.
-
-    Cette fonction utilise une approche de
-    programmation dynamique pour déterminer
-    le nombre minimum de pièces nécessaires
-    pour former un montant total avec un
-    ensemble donné de pièces. Si le montant
-    ne peut pas être atteint avec les
-    pièces disponibles, la fonction
-    retourne -1. Si le montant total est 0 ou
-    inférieur, la fonction retourne 0.
+    Détermine le nombre minimum de pièces nécessaires
+    pour obtenir un montant total donné
+    lorsqu'on dispose d'un ensemble de pièces de différentes
+    valeurs. Utilise un algorithme glouton
+    en choisissant toujours la pièce de plus grande valeur disponible.
 
     Paramètres:
-    coins (list of int): Liste des dénominations des
-                        pièces disponibles. Chaque
-                        valeur dans la liste représente
-                        une dénomination de pièce
-                        et il y a une quantité illimitée
-                        de chaque type de pièce.
-    total (int): Montant total que l'on souhaite
-                former avec les pièces. Il doit
-                être un entier non négatif.
+    coins (list of int): Liste d'entiers représentant
+    les valeurs des pièces disponibles.
+    total (int): Montant total à atteindre.
 
     Retourne:
-    int: Nombre minimum de pièces nécessaires
-    pour former le montant total.
-         Retourne -1 si le montant total
-         ne peut pas être formé avec les pièces
-         disponibles. Retourne 0 si le montant
-         total est 0 ou inférieur.
-
-    Exemple:
-    >>> makeChange([1, 2, 25], 37)
-    7
-    >>> makeChange([1256, 54, 48, 16, 102], 1453)
-    -1
+    int: Le nombre minimum de pièces nécessaires pour
+    obtenir le montant total. Retourne -1
+         si le montant total ne peut pas être atteint
+         avec les pièces disponibles.
 
     Notes:
-    - La fonction utilise une liste `vdb` où `vdb[i]`
-        représente le nombre minimum de
-        pièces nécessaires pour obtenir le montant `i`.
-    - La complexité en temps de l'algorithme
-        est O(n * m), où n est le nombre de
-        dénominations de pièces et m est le montant total.
-    - La complexité en espace est O(m),
-    pour stocker la liste `vdb`.
+    - Cette implémentation utilise un algorithme glouton,
+    qui peut ne pas fonctionner dans tous les cas.
+      Par exemple, avec des pièces {1, 3, 4} et un montant
+      de 6 (la solution correcte est 2 pièces: 3+3),
+      l'algorithme glouton retournerait 3 pièces: 4+1+1.
+    - La complexité temporelle est O(n log n) en raison du tri
+    des pièces, où n est le nombre de dénominations de pièces.
     """
-    # Cas de base : si le total est 0 ou négatif,
-    # aucun jeton n'est nécessaire
+
+    # Cas particulier : si le montant total est 0 ou inférieur
     if total <= 0:
         return 0
 
-    # Initialiser une liste pour stocker le nombre
-    # minimum de pièces nécessaires pour chaque montant
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-    # Zéro pièce nécessaire pour obtenir un total de 0
+    # Trie les pièces par ordre décroissant
+    sortdCoins = sorted(coins, reverse=True)
+    
+    # Initialisation des variables
+    rst = total  # Montant restant à obtenir
+    coinsCount = 0  # Compteur pour le nombre de pièces utilisées
 
-    # Itérer à travers chaque pièce et mettre à jour le tableau dp
-    for coin in coins:
-        for x in range(coin, total + 1):
-            # Mettre à jour le nombre minimum de pièces
-            # nécessaires pour obtenir le montant x
-            dp[x] = min(dp[x], dp[x - coin] + 1)
+    # Processus pour obtenir la monnaie
+    for cn in sortdCoins:
+        while rst >= cn:
+            rst -= cn
+            coinsCount += 1
 
-    # Si le total ne peut pas être atteint, retourner -1
-    return dp[total] if dp[total] != float('inf') else -1
+    # Si après avoir utilisé toutes les pièces
+    # disponibles, il reste encore un montant
+    if rst > 0:
+        return -1
+
+    return coinsCount
